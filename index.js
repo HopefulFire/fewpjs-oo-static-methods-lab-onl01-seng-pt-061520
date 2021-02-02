63386ac321c0dbd9a09050252992eedcbf9a3ef7
Jade Lynn Masker <jadelynn@masker.family>
2021-02-01 21:53:44 -0500

    commit tree 5e502e8ad0ff9480e599bae5713f270975cc6222

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+	static capitalize(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
+	static sanitize(string) {
+		return string.replace(/[^A-Za-z0-9 '-]/g, '');
+	}
+
+	static titleize(string) {
+		return this.capitalize(string).split(" ").map((word) => {
+			if ((/^(the|a|an|but|of|and|for|at|by|from)$/g).test(word)) {
+				return word;
+			} else {
+				return this.capitalize(word);
+			}
+		}).join(" ");
+	}
 }
